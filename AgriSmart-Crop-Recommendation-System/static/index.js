210f231245c8ff588856a7444b7e8ed91ba2050f
Thecouchking <125556942+Thecouchking@users.noreply.github.com>
2024-11-28 02:30:48 +0530

    agrismart

---
+++ AgriSmart-Crop-Recommendation-System/static/index.js
@@ -0,0 +1,12 @@
+const background = document.querySelector('.background-3d');
+
+window.addEventListener('mousemove', (event) => {   
+    const { clientX, clientY } = event;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    const offsetX = (clientX - centerX) / 50;
+    const offsetY = (clientY - centerY) / 50;
+
+  background.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(1.1)`;
+});
